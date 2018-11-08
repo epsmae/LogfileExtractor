fa63a5d6dd7386ec871c4046b5186c66993ade79
Manuel Eugster <manuel.eugster@hispeed.ch>
2018-11-08 21:42:19 +0100

    Added command line project

---
+++ Develop/LogFileExtractor.App/Options.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace LogFileExtractor.App
+{
+    internal class Options
+    {
+        [Option('f', "logfile", Required = true, HelpText = "Logfile to extract")]
+        public string LogFile { get; set; }
+
+        [Option('t', "tag", Required = true, HelpText = "Tag to extract lines")]
+        public string Tag { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Csv output file path")]
+        public string Output { get; set; }
+    }
+}
---
+++ Develop/LogFileExtractor.App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+using LogfileExtractor.DataModel;
+using LogfileExtractor.Export;
+
+namespace LogFileExtractor.App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(RunOptionsAndReturnExitCode)
+                .WithNotParsed<Options>(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> error)
+        {
+
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options options)
+        {
+            try
+            {
+                LogfileExtractor.Service.LogFileExtractor logFileExtractor = new LogfileExtractor.Service.LogFileExtractor(options.LogFile);
+                IList<LineItem> items = logFileExtractor.Extract(options.Tag);
+
+                CsvExporter exporter = new CsvExporter();
+                exporter.Export(options.Output, items);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to extract logfile" + ex);
+            }
+
+        }
+    }
+}
