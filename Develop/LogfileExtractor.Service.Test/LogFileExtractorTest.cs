4e781672b73c95bdc2050c61261930f24a3d92f2
Manuel Eugster <manuel.eugster@hispeed.ch>
2018-11-08 20:26:58 +0100

    Added logfile extractor

---
+++ Develop/LogfileExtractor.Service.Test/LogFileExtractorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace LogfileExtractor.Service.Test
+{
+    [TestFixture]
+    public class LogFileExtractorTest
+    {
+        private const string Tag = "__--Tag--__";
+        private string TestDirectory
+        {
+            get { return Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData"); }
+        }
+
+        private string TestFile
+        {
+            get { return Path.Combine(TestDirectory, "logfile.log"); }
+        }
+
+        [Test]
+        public void TestExtractLogFile()
+        {
+            LogFileExtractor extractor = new LogFileExtractor(TestFile);
+
+            IList<LineItem> items = extractor.Extract(Tag);
+            Assert.AreEqual(7, items.Count);
+            Assert.AreEqual(5, items[0].Items[0].Column);
+            Assert.AreEqual("this_is_important", items[0].Items[0].Value);
+            Assert.AreEqual(8, items[6].Items[0].Column);
+            Assert.AreEqual("another_message", items[6].Items[0].Value);
+        }
+    }
+}
---
+++ Develop/LogfileExtractor.Service/LogFileExtractor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogfileExtractor.Service
+{
+    public class LogFileExtractor
+    {
+        private readonly TagFinder _finder;
+        private readonly LineExtractor _lineExtractor;
+
+        public LogFileExtractor(string logFile)
+        {
+            _finder = new TagFinder(logFile);
+            TableItemExtractor itemExtractor = new TableItemExtractor();
+            _lineExtractor  = new LineExtractor(itemExtractor);
+        }
+
+        public IList<LineItem> Extract(string tag)
+        {
+            IList<LineItem> items = new List<LineItem>();
+            IList<string> lines = _finder.FindTaggedLines(tag);
+
+            foreach (string line in lines)
+            {
+                try
+                {
+                    LineItem item = _lineExtractor.ExtractLine(line);
+                    items.Add(item);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            return items;
+        }
+    }
+}
